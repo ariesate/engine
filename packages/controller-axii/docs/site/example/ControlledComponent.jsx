71c1b687aa9216b76f4fba4518afccf1af80b4f0
sskyy <skyking_H@hotmail.com>
2021-04-09 11:47:28 +0800

    feat: move site to axii-site

--- packages/controller-axii/docs/site/example/ControlledComponent.jsx
+++
@@ -1,9 +0,0 @@
-/** @jsx createElement */
-import { createElement, ref, propTypes } from 'axii'
-import Input from './Input.jsx'
-
-export default function ControlledComponent() {
-  const value = 'controlled value'
-  return <Input value={value}/>
-}
-
