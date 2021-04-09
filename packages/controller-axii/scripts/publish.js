da98913ec8df15e832fe58e1f13f35143b76546a
sskyy <skyking_H@hotmail.com>
2021-04-09 10:40:53 +0800

    version: axii undefined

---
+++ packages/controller-axii/scripts/publish.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import commandLineArgs from 'command-line-args'
+import util from 'util'
+import childProcess from 'child_process'
+import {readFile} from "fs/promises";
+const exec = util.promisify(childProcess.exec)
+
+async function loadJSON(userPath, fs = { readFile }) {
+  return JSON.parse(await fs.readFile(new URL(userPath, import.meta.url)))
+}
+
+const optionDefinitions = [
+  { name: 'patch', alias: 'p', type: Boolean },
+  { name: 'minor', alias: 'm', type: Boolean },
+  { name: 'major', alias: 'M', type: Boolean }
+]
+
+const options = commandLineArgs(optionDefinitions, { partial: true })
+
+const versionType = options.major ? 'majon' : (options.minor ? 'minor' : 'patch')
+
+await exec(`npm version ${versionType}`)
+await exec(`npm run build`)
+console.log(loadJSON(path.join(process.cwd(), './package.json')))
+const { version } = loadJSON(path.join(process.cwd(), './package.json'))
+await exec(`git add .`)
+await exec(`git commit -m "version: axii ${version}"`)
+await exec('npm publish ./ --access public')
+
+
