7f4c43b6995eabb6fed3f034d39a32e2b51099c2
sskyy <skyking_H@hotmail.com>
2021-04-10 21:09:58 +0800

    docs: update Table doc

--- packages/axii-components/src/table/Table.jsx
+++ packages/axii-components/src/table/Table.jsx
@@ -12,35 +12,6 @@ import Selectable from './Selectable'
 import StickyLayout from './StickyLayout'
 
 import scen from '../pattern'
-/**
- * // TODO
- * 1. 视窗
- *  1.1 翻页
- *  1.2 筛选
- *  1.3 过滤
- * 2. 编辑
- * 4. 行列合并
- * 5. refComputed
- */
-
-/**
- * feature 体系，改结构的问题：
- * 1. 只能以 fragment 的方式扩展(selectable/expandable)
- * 2. 或者以 overwrite 的方式来拓展实现
- *
- * 改位置 box 信息？
- * 1. TODO 只要当前结构允许，就可以???fix column 怎么实现？
- *
- * 应该有一种范式，不管组件是什么样的，在这个范式下，改布局，改结构就能实现。
- * TODO 改结构、改位置的需求也可以继续探索本质？？？
- * 1. 我们的数据结构只有树形，进行渲染的逻辑也只有树形。
- * 2. 通过树形来渲染应该是一种领域知识。
- * 3. fixHeader 的 overwrite 没有破坏树形渲染的过程
- * 4. fixColumn 破坏了，因为本来没有 column 这样一个连接，只有 row。
- *
- * 5. 处分 table 把每个部分都虚拟化。才能实现对 column 的控制。
- */
-
 
 function walkLeaf(nodes, handle) {
   nodes.forEach(node => {
@@ -51,7 +22,6 @@ function walkLeaf(nodes, handle) {
     }
   })
 }
-
 
 function readByLevel(nodes, level, handle) {
   let levelIndex = 0
@@ -123,8 +93,6 @@ export function Table( { data, pagination, columns }, fragments) {
   )
 }
 
-
-// TODO layout 中统一控制的部分怎么处理？？
 export const thStyle = {
   borderColor: scen().separateColor(),
   borderStyle: 'solid',
@@ -147,7 +115,6 @@ Table.propTypes = {
   columns: propTypes.array.default(() => reactive([]))
 }
 
-// 应该写成这个形式
 export default createComponent(Table, [StickyLayout, Selectable, Expandable])
 
 
