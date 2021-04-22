96328e81e53a6fef29452109accbd62c3cf425e5
sskyy <skyking_H@hotmail.com>
2021-04-22 16:36:37 +0800

    chore: change ref to atom in axii

--- packages/axii-icons/src/TwoHands.jsx
+++ packages/axii-icons/src/TwoHands.jsx
@@ -4,12 +4,12 @@ import Icon from '@icon-park/svg/es/icons/TwoHands.js';
 import {
   createElement,
   propTypes,
-  refComputed,
-  ref
+  atomComputed,
+  atom
 } from 'axii';
 
 export default function IconPark({ wrapper, size, unit, theme, fill, strokeLinecap, strokeLinejoin, strokeWidth, ...rest }) {
-  const iconStr = refComputed(() => ({
+  const iconStr = atomComputed(() => ({
     __html: Icon({
       theme: theme.value,
       size: `${size.value}${unit.value}`,
@@ -24,15 +24,15 @@ export default function IconPark({ wrapper, size, unit, theme, fill, strokeLinec
 }
 
 IconPark.propTypes = {
-  size: propTypes.number.default(() => ref(1)),
-  unit: propTypes.string.default(() => ref('em')),
+  size: propTypes.number.default(() => atom(1)),
+  unit: propTypes.string.default(() => atom('em')),
   // theme: outline filled two-tone multi-color
-  theme: propTypes.string.default(() => ref('outline')),
-  fill: propTypes.string.default(() => ref('#333')),
+  theme: propTypes.string.default(() => atom('outline')),
+  fill: propTypes.string.default(() => atom('#333')),
   //'butt' | 'round' | 'square'
-  strokeLinecap: propTypes.string.default(() => ref('round')),
+  strokeLinecap: propTypes.string.default(() => atom('round')),
   // 'miter' | 'round' | 'bevel'
-  strokeLinejoin: propTypes.string.default(() => ref('round')),
-  strokeWidth: propTypes.number.default(() => ref(4)),
-  wrapper: propTypes.string.default(() => ref('iconWrapper')),
+  strokeLinejoin: propTypes.string.default(() => atom('round')),
+  strokeWidth: propTypes.number.default(() => atom(4)),
+  wrapper: propTypes.string.default(() => atom('iconWrapper')),
 }
