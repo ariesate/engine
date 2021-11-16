c5aab80f77f3fe467f65feb8fc7aeae2fa210517
zhouyunge <zhouyunge@kuaishou.com>
2021-11-16 17:21:32 +0800

    fix: useViewEffect only recive normal function

--- packages/axii-components/playground/playground.jsx
+++ packages/axii-components/playground/playground.jsx
@@ -11,26 +11,28 @@ const { component } = location.query;
 function ExampleCode() {
   const codeContainerRef = useRef();
 
-  useViewEffect(async () => {
-    await import(`./demos/${component}.jsx`);
-    if (component) {
-      const promise = import.meta.env.DEV
-        ? import(`./demos/${component}.jsx?raw`)
-        : import(`./demos-assets/${component}.jsx`);
-      promise
-        .then(({ default: content }) => {
-          const flask = new CodeFlask(codeContainerRef.current, {
-            language: "js",
-            readonly: true,
+  useViewEffect(() => {
+    (async () => {
+      await import(`./demos/${component}.jsx`);
+      if (component) {
+        const promise = import.meta.env.DEV
+          ? import(`./demos/${component}.jsx?raw`)
+          : import(`./demos-assets/${component}.jsx`);
+        promise
+          .then(({ default: content }) => {
+            const flask = new CodeFlask(codeContainerRef.current, {
+              language: "js",
+              readonly: true,
+            });
+            flask.updateCode(
+              import.meta.env.DEV ? content : decodeURIComponent(content)
+            );
+          })
+          .catch((e) => {
+            console.error(e);
           });
-          flask.updateCode(
-            import.meta.env.DEV ? content : decodeURIComponent(content)
-          );
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    }
+      }
+    })();
   });
 
   return (
