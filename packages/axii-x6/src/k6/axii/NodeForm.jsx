b4ec64084ae262e2255d991921f0abe9c3dbce51
zhouyunge <zhouyunge@kuaishou.com>
2021-11-26 14:48:12 +0800

    feat: fix top data reredner

--- packages/axii-x6/src/k6/axii/NodeForm.jsx
+++ packages/axii-x6/src/k6/axii/NodeForm.jsx
@@ -1,32 +1,74 @@
+/** @jsx createElement */
 import {
+  tryToRaw,
   createElement,
   createComponent,
-  useContext,
+  createContext,
+  reactive,
   useViewEffect,
+  useContext,
+  watch,
+  delegateLeaf,
+  traverse,
+  atom,
+  computed,
 } from 'axii';
 
 import { RootContext } from './Root';
-import DataConfig from './DataConfig';
-import { merge } from 'lodash';
+import DataConfig, { mergeJsonAndData, fallbackEditorDataToNormal } from './DataConfig';
+import { merge, cloneDeep } from 'lodash';
 
 function NodeForm(props) {
   const context = useContext(RootContext);
 
+  const formJson = atom(null);
 
-  function onSave(rawSelectedData) {
+  window.formJson = formJson;
+
+  const showConfigForm = computed(() => {
+    return !!context.dm.insideState.selectedConfigJSON && !!context.dm.insideState.selectedConfigData;
+  });
+
+  function onSave(rawSelectedData) {    
     const { selectedCellId, selectedConfigData, selectedConfigJSON } = context.dm.insideState;
     const latestData = merge(selectedConfigData, rawSelectedData);
     
-    context.dm.triggerCurrentEvent('change', latestData);
+    context.dm.triggerCurrentEvent('change', latestData);    
   }
 
   function onChange(rawSelectedData) {
-    
+    const { selectedCellId, selectedConfigData, selectedConfigJSON } = context.dm.insideState;
+    merge(selectedConfigData, rawSelectedData);
+    context.dm.triggerCurrentEvent('change', selectedConfigData);    
   }
 
+  useViewEffect(() => {
+    watch(() => showConfigForm.value, () => {
+      if (showConfigForm.value) {
+        setTimeout(() => {
+          const json = context.dm.insideState.selectedConfigJSON;
+          const data = tryToRaw(context.dm.insideState.selectedConfigData);
+          const mergedJson = mergeJsonAndData(json, data);
+          formJson.value = cloneDeep(mergedJson);
+        });
+      } else {
+        formJson.value = null;
+      }
+    });
+  });
+
+  console.log('nodeForm render');
+
   return (
-    <DataConfig onChange={onChange} onSave={onSave}></DataConfig>
-  );
+    <nodeForm>
+      {(() => {
+        if (!formJson.value) {
+          return '';
+        }
+        return (<DataConfig jsonWithData={formJson.value} onChange={onChange} onSave={onSave}></DataConfig>);  
+      })}
+    </nodeForm>
+  )
 }
 
 export default createComponent(NodeForm);
